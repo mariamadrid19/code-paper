0409c127ae64a2aedb36546bcc855a950a9dce7d
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2023-07-12 14:37:16 +0200

    Update correlation_plots.R

--- metabolomics/correlation_plots.R
+++ metabolomics/correlation_plots.R
@@ -33,3 +33,53 @@ corrplot(cor_matrix_green_its,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
 #correlation plot, roasted coffee vs yeast
 cor_matrix_roasted_its <- cor(correlation_its,correlation_roasted, method = "spearman")
 corrplot(cor_matrix_roasted_its,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
+
+#Correlation plot, roasted coffee vs all mos, IW
+corr_mos_RIW <- as.data.frame(corr_mos_RIW)
+rownames(corr_mos_RIW) <- corr_mos_RIW$Sample
+corr_mos_RIW <- corr_mos_RIW[,-1]
+str(corr_mos_RIW)
+corr_RIW <- as.data.frame(corr_RIW)
+rownames(corr_RIW) <- corr_RIW$Sample
+corr_RIW <- corr_RIW[,-1]
+str(corr_RIW)
+cor_matrix_all_mos_RIW <- cor(corr_mos_RIW,corr_RIW, method = "spearman")
+corrplot(cor_matrix_all_mos_RIW,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
+
+#Correlation plot, roasted coffee vs all mos, SW
+corr_mos_RSW <- as.data.frame(corr_mos_RSW)
+rownames(corr_mos_RSW) <- corr_mos_RSW$Sample
+corr_mos_RSW <- corr_mos_RSW[,-1]
+str(corr_mos_RSW)
+corr_RSW <- as.data.frame(corr_RSW)
+rownames(corr_RSW) <- corr_RSW$Sample
+corr_RSW <- corr_RSW[,-1]
+str(corr_RSW)
+cor_matrix_all_mos_RSW <- cor(corr_mos_RSW,corr_RSW, method = "spearman")
+corrplot(cor_matrix_all_mos_RSW,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
+
+
+#Correlation plot, green coffee vs all mos, IW
+corr_mos_GIW <- as.data.frame(corr_mos_GIW)
+rownames(corr_mos_GIW) <- corr_mos_GIW$Sample
+corr_mos_GIW <- corr_mos_GIW[,-1]
+str(corr_mos_GIW)
+corr_GIW <- as.data.frame(corr_GIW)
+rownames(corr_GIW) <- corr_GIW$Sample
+corr_GIW <- corr_GIW[,-1]
+str(corr_GIW)
+cor_matrix_all_mos_GIW <- cor(corr_mos_GIW,corr_GIW, method = "spearman")
+corrplot(cor_matrix_all_mos_GIW,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
+
+#Correlation plot, green coffee vs all mos, SW
+corr_mos_GSW <- as.data.frame(corr_mos_GSW)
+rownames(corr_mos_GSW) <- corr_mos_GSW$Sample
+corr_mos_GSW <- corr_mos_GSW[,-1]
+str(corr_mos_GSW)
+corr_GSW <- as.data.frame(corr_GSW)
+rownames(corr_GSW) <- corr_RIW$Sample
+corr_GSW <- corr_GSW[,-1]
+str(corr_GSW)
+cor_matrix_all_mos_RSW <- cor(corr_mos_GSW,corr_GSW, method = "spearman")
+corrplot(cor_matrix_all_mos_RSW,tl.cex = 0.6,tl.col="black",is.corr=FALSE)
+
